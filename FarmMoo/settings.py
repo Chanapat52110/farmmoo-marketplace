94ce70a45d0be508953281160aca299da6f21b9f
Chanapat52110 <65991010@kmitl.ac.th>
2026-05-07 02:16:45 +0700

    fix allow any

--- FarmMoo/settings.py
+++ FarmMoo/settings.py
@@ -183,7 +183,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'EXCEPTION_HANDLER': 'FarmMoo.exception_handler.custom_exception_handler',
 }
